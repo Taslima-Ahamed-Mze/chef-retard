7b00e04bb2dd15ee1f4d3487c94749ce771bbbc9
Taslima-Ahamed-Mze <taslimaahamedmze@gmail.com>
2023-12-02 22:52:52 +0100

    format Site map

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -12,7 +12,8 @@ export default function sitemap(): MetadataRoute.Sitemap {
       lastModified: post.lastmod || post.date,
     }))
 
-  const routes = ['', 'categories', 'conseils', 'contact', 'categories/cuisine-reconfortante'].map((route) => (
+  const routes = ['', 'categories', 'conseils', 'contact', 'categories/cuisine-reconfortante'].map(
+    (route) => (
     {
       url: `${siteUrl}/${route}`,
       lastModified: new Date().toISOString().split('T')[0],
